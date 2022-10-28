0466396397be64080dd35c9b65f2bcf605b59e7d
Mitchell <mitchel2206@gmail.com>
2022-10-28 16:03:24 -0300

    cryto

---
+++ src/componets/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import "./style.css"
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li><a href="">PRODUCTOS</a></li>
+                <li><a href="">ARMA TU PC</a></li>
+                <li><a href="">MARCAS</a></li>
+                <li><a href="">AYUDA</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
